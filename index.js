1ccb96b656dba1d942fbb564925404f949617c8c
sanriqing <719755851@qq.com>
2018-05-01 00:31:29 +0800

    删除无用的内容

--- index.js
+++
@@ -1,8 +0,0 @@
-const download = require('download')
-download('github.com/kevva/download.git', process.cwd(), {
-    filename: 'ceshi'
-}).then(() => {
-    console.log('i has download')
-}).catch(e => {
-    console.log(e)
-})
